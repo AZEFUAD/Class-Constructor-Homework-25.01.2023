9e92c2d97c2b3076f5f703821eb317abed074893
AZEFUAD <fuadkhs@code.edu.az>
2023-03-05 22:08:01 +0400

    Successfully committed.

---
+++ Class,Constructor Homework 25.01.2023/Class,Constructor Homework 25.01.2023/Program.cs
@@ -0,0 +1,21 @@
+﻿using Class_Constructor_Homework_25._01._2023;
+using System;
+
+namespace Class_Constructor_Homework_25._01._2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal1 = new Animal();
+            animal1.name = "Rex";
+            animal1.breed = "Belgian Malinois";
+            animal1.color = "Brown";
+            animal1.age = 4;
+
+            Console.WriteLine(animal1.GetDetails());
+            Building building1 = new Building("Park Dede Qorqud", 24, 48);
+            building1.GetVolume();
+        }
+    }
+}
